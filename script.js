634651d6b767a61fe5e1a2cc412fc9ae69643403
rafael1102 <rafa.ro195@gmail.com>
2023-01-21 15:23:10 -0300

    coretion
    Signed-off-by: rafael1102 <rafa.ro195@gmail.com>

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const form = document.querySelector("#form-habits")
-const   nlwSetup = new nlwSetup(form)
+const nlwSetup = new NLWSetup(form)
 const button = document.querySelector("header button")
 
 button.addEventListener("click", add)
-form.addEventListener('change', save)
+form.addEventListener("change", save)
 
 function add() {
-    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
-    const dayexists = nlwSetup.dayExists(today)
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
 
-    if(dayexists){
-        alert("Dia já incluso 🔴")
-        return
-    }
+  if (dayExists) {
+    alert("Dia já incluso 🔴")
+    return
+  }
 
-    alert("Dia adicionado com sucesso ✅")
-    nlwSetup.addDay(today)
+  alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
 }
 
-
-function save () {
-    localStorage.setItem('NLWSetup@habits',JSON.stringify(nlwSetup.data))
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
-const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
